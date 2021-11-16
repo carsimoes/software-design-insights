525e5b6892a32d8787991d202f978fcba7ba056b
BIZ\RenatCar <carlos.simoes@electrolux.com>
2021-11-16 10:58:36 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace software_design_insights
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ValueObject/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace software_design_insights.ValueObject
+{
+    public class Customer
+    {
+        public Guid CustomerId { get; set; }
+        public string Name { get; set; }
+        public decimal Discount { get; set; }
+        public Address Address { get; set; }
+    }
+
+    public record Address(string Street, string City, string State, string Country, string PostalCode);
+}
